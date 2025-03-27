9114fde8c9b1143c90a0669beb5eed9a17e8c733
Ronit9320 <shahronit16@gmail.com>
2025-03-27 23:28:00 +0530

    setups

--- main.lua
+++ main.lua
@@ -1,3 +1,17 @@
+function love.load()
+  x = 400
+  y = 300
+  speed = 200
+end
+
+function love.update(dt)
+  if love.mouse.isDown(1, 0) then
+    print("mouse button pressed")
+  end
+end
+
+
 function love.draw()
-  love.graphics.print("Hello, World", 400, 300)
+  love.graphics.setColor(1.0, 0, 0)
+  love.graphics.rectangle("fill", x, y, 50, 50)
 end
